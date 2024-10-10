d3d49f7ce8abc9378bcf7adc490f584020707ee7
Michal Lewandowski <michalewy00@gmail.com>
2024-10-10 23:51:32 +0200

    edited sentence_tokenize()

--- data_processing/processing.py
+++ data_processing/processing.py
@@ -4,11 +4,13 @@ import emoji
 from urllib.parse import urlparse
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import WordNetLemmatizer
 
 import nltk
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 
 class DataCleaning:
 
@@ -16,7 +18,7 @@ class DataCleaning:
         self.website = website
         video_id = self.get_video_id()
         self.data = api.get_video_comments(video_id)
-
+        self.lemmatizer = WordNetLemmatizer()
 
     def get_video_id(self):
         parsed_url = urlparse(self.website)
@@ -30,11 +32,15 @@ class DataCleaning:
 
         for comment in self.data:
             
-            preprocesed_text = comment.lower()  
-            preprocesed_text = re.sub(r'\b\w{1}\b', '', preprocesed_text)  
-            preprocesed_text = re.sub(r'\s+', ' ', preprocesed_text)  
-            preprocesed_text = re.sub(r'[^\w\s]', '', preprocesed_text) 
-            clean_data.append(preprocesed_text)
+            preprocessed_text = comment.lower()  
+            preprocessed_text = re.sub(r'\b\w{1}\b', '', preprocessed_text)  
+            preprocessed_text = re.sub(r'\s+', ' ', preprocessed_text)  
+            preprocessed_text = re.sub(r'[^\w\s]', '', preprocessed_text) 
+
+            preprocessed_text = emoji.replace_emoji(preprocessed_text, replace='')
+            preprocessed_text = re.sub(r'http\S+|www.\S+', '', preprocessed_text)
+            
+            clean_data.append(preprocessed_text)
 
         return clean_data
 
@@ -43,14 +49,15 @@ class DataCleaning:
         filtered_sentences = []
 
         for sentence in data:
+            
             words = word_tokenize(sentence)
            
             filtered_sentence = ' '.join([word for word in words if word not in set(stopwords.words("english"))]) 
-            filtered_sentences.append(filtered_sentence)
 
-        for i in filtered_sentences:
-            if len(i) == 0:
-                filtered_sentences.remove(i)
+            lemmatized_sentence = ' '.join([self.lemmatizer.lemmatize(word) for word in word_tokenize(filtered_sentence)])
+
+            if len(lemmatized_sentence) > 0:
+                filtered_sentences.append(lemmatized_sentence)
 
         return filtered_sentences
     
