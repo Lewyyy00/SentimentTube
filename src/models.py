0a9e82bfa97107f39440c637ca4b485e615b466f
Michal Lewandowski <michalewy00@gmail.com>
2024-10-13 22:15:53 +0200

    added new method

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from data_processing import processing
 from sklearn.feature_extraction.text import TfidfVectorizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-    
+
 class YouTubeCommentAnalyzer:
 
     def __init__(self, website):
@@ -15,5 +15,22 @@ class YouTubeCommentAnalyzer:
         tfidf_matrix = self.vectorizer.fit_transform(self.comments)
         
         return tfidf_matrix
-        
+    
+    def analyze_sentiment(self):
+      
+        analyzer = SentimentIntensityAnalyzer()
+        sentiment_labels = []
+
+        for comment in self.comments:
+            sentiment_scores = analyzer.polarity_scores(comment)
+            if sentiment_scores['compound'] >= 0.05:
+                sentiment_labels.append('positive')
+            elif sentiment_scores['compound'] <= -0.05:
+                sentiment_labels.append('negative')
+            else:
+                sentiment_labels.append('neutral')
+
+        return sentiment_labels
   
+x = YouTubeCommentAnalyzer('https://www.youtube.com/watch?v=5eqRuVp65eY').analyze_sentiment()
+print(x)
