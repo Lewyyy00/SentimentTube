6cd5880824b398716e2af0d4e7d73295b5e2e1d0
Michal Lewandowski <michalewy00@gmail.com>
2024-10-30 23:53:07 +0100

    added multithreading

--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
 from data_processing import processing
+import os
 from data_processing import api
 from sklearn.feature_extraction.text import TfidfVectorizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from collections import Counter
 import matplotlib.pyplot as plt
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class YouTubeCommentAnalyzer:
 
@@ -14,6 +16,7 @@ class YouTubeCommentAnalyzer:
         self.comments = self.data_cleanner.sentence_tokenize() 
         self.sentiment_data = self.analyze_sentiment()
         self.video_detalis = self.data_cleanner.otherapidata
+        #self.max_workers = os.cpu_count()
 
     def vectorize_data(self, max_features=5000):
 
@@ -23,10 +26,10 @@ class YouTubeCommentAnalyzer:
         return tfidf_matrix
     
     def analyze_sentiment(self):
-
+        
         sentiment_labels = []
 
-        for comment in self.comments:
+        def analyze_comment(comment):
             sentiment_scores = self.analyser.polarity_scores(comment)
             if sentiment_scores['compound'] >= 0.05:
                 sentiment_labels.append('positive')
@@ -35,8 +38,20 @@ class YouTubeCommentAnalyzer:
             else:
                 sentiment_labels.append('neutral')
 
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            
+            futures = {executor.submit(analyze_comment, comment): comment for comment in self.comments}
+
+            for future in as_completed(futures):
+                try:
+                    result = future.result()  
+                    sentiment_labels.append(result)
+                except Exception as exc:
+                    print(f'Comment generated an exception: {exc}')
+
         return Counter(sentiment_labels)
     
+
     def bar_chart_maker(self):
 
         sentiment_count = self.sentiment_data
